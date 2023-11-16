8b1ed101fd02238f4b36877f8cdba8042f39ab1b
DIST1 AUDAC <DST1@AUDAC.local>
2023-11-16 18:54:44 +0100

    Test#02: Titles and Input.

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -13,9 +13,21 @@ import styles from './welcome.style'
 import {icons, SIZES} from '../../../constants';
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello, Mesaque</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+          />
+        </View>
+      </View>
     </View>
   )
 }
